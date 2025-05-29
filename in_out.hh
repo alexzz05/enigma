cffcf5ac4b1d9ea22c049067d0eae973ac183999
polsese1 <pol.sese@enti.cat>
2025-05-29 12:06:26 +0200

    Add files via upload

---
+++ in_out.hh
@@ -0,0 +1,8 @@
+#include <limits>
+#include <fstream>
+#include "utilidades.hh"
+
+bool crearArchivoRotor(const string& path, ifstream& entrada);
+void leerRotor(const string& path);
+void comprobarRotores();
+void modificarRotores();
